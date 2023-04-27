efad50b353cd8f02f7edb610de0ecd8f42dfcdf6
nagachen <birdann99@gmail.com>
2023-04-27 11:34:47 +0800

    GET及POST資料的拿取判斷

--- target.php
+++ target.php
@@ -1,8 +1,28 @@
 <h2>目標網頁<h2>
-<?php           
+<?php  
 #所有傳遞的資料皆是字串;
 #一個表單只能用一種方式傳送
- echo $_GET['addr'];            
- echo $_POST['name']; 
- echo $_POST['type'];           
-echo $_post['img'];
+
+//  echo $_GET['addr'];            
+//  echo $_POST['name']; 
+//  echo $_POST['type'];           
+// echo $_post['img'];
+
+
+#需注意empty()....相關的判定, 參考謝晒的網頁 
+#$_GET(),$_POST()為apache 系統預設變數
+#需注意數值為0的情況
+if(!empty($_GET)){
+    echo "以下資料為GET表單的資料<br>";
+    echo"<pre>";
+    print_r($_GET);
+    echo"<pre>";
+
+}
+if(!empty($_post)){
+    echo "以下資料為post表單的資料<br>";
+    echo"<pre>";
+    print_r($_post);
+    echo"<pre>";
+
+}
