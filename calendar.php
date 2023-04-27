dfa75e988ee1f5ab757652230e09db246d636947
nagachen <birdann99@gmail.com>
2023-04-27 23:39:45 +0800

    萬年歷實作到flexbox階段

--- calendar.php
+++ calendar.php
@@ -1,67 +1,72 @@
-<link rel="stylesheet" href="style.css">
-<h2>月曆</h2>
+<!-- 萬年歷 -->
 
-<?php
-#目前問題：
-
-#2.休假日的判定，是不是可以和國定假日六，用同一種判定方法，再統一填色？
-#3.第一週和最後一週將日期（上個月和下個月的日期填入，並以虛線顯示）
-#4.年和月切換的按鈕制作，（未教）
-$show_month=(isset($_GET['month']))?$_GET['month']:date("n");                                 #模擬輸入月份
-$year=$_GET['year']??date("Y"); #取得今年數宇
-$show_day=strtotime("Y-.$show_month"."-l");  #模擬日數預設為1日
-
-// echo"<br>";
-// echo$show_day;
-// $today=strtotime("now");                        #現在秒數
-// $month=date("n",$today);                        #取出目前月份
-
-$days=date("t",$show_day);                         #取出月份天數
-$firstDate=date("Y-n-1",$show_day);                #顯示第一天的日期
-$finalDate=date("Y-n-t",$show_day);                #顯是最後一天的日期
-$firstDateWeek=date("w",strtotime($firstDate)); #第一週星期幾
-$finalDateWeek=date("w",strtotime($finalDate)); #最一週星期幾
-$weeks=ceil(($days+$firstDateWeek)/7);          #要隔多少週
-$firstWeekSpace=$firstDateWeek-1;               #6-1,第一週第一天會是6所以6-1
-
-
-
-
-//先畫出固定的表頭內容
-
-$days=[];
-
-//使用迴圈來畫出當前月的周數
-for($i=0;$i<$weeks;$i++){
-    //使用迴圈來畫出當周的天數
-    for($j=0;$j<7;$j++){
-        //判斷當周是否為第一周或最後一周
-        if(($i==0 && $j<$firstDateWeek) || (($i==$weeks-1) && $j>$finalDateWeek)){
-            $days[]='&nbsp';
-        }else{
-            $days[]=$j+7*$i-$firstWeekSpace;
-        }
-    }
-}
-
-
-
-#使用flexbox
-?>
-
-<div class='calendar'>
-<div>日</div>
-<div>一</div>
-<div>二</div>
-<div>三</div>
-<div>四</div>
-<div>五</div>
-<div>六</div>
-<?php
-for($i=0;$i<count($days);$i++){
-    echo "<div> {$days[$i]} </div>";  /* 大括號內只能字串，不能做數值相加*/
-}
-
-?>
-
+<?php 
+#取得相關資料
+$today=strtotime('now');    #目前時間
+$month=date("n",$today);#月歷月份
+$year=date("Y"); #目前年份
+$first_day=strtotime("$year-$month-1");     #當月的第一天秒數
+$days=date("t",$today);                      #取得最後一天，天數
+$final_day=strtotime("$year-$month-$days",$today);     #當月的最後一天秒數
+$first_week=date('w',$first_day);           #第一週星期幾 六
+$final_week=date('w',$final_day);          #最後一週星期幾 日
+$month_days = ($days + $first_week);      #當月總共要算幾天36
+$month_week = ceil($month_days/7);           #當月有幾週 6
+echo $month_week;
+echo"<br>";
+echo $days;
+    echo "<br>";
+echo $first_week;
+echo "<br>";
+echo $final_day;
+echo "<br>";
+#建立月份陣列相關資料
+$data = [];
+ for($i=0;$i<$month_week;$i++){
     
+      for($j=0;$j<7;$j++){
+          #判斷要寫入空白還是天數
+          if(($j<$first_week && $i == 0) ||($i==($month_week-1) && $final_week<$j)){
+              $data[]="&nbsp;";
+          }else{
+              $data[]=($i*7)+$j-$first_week+1;
+    
+          }
+      }
+ }
+// echo "<pre>";
+// print_r($data);
+// echo "</pre>";
+
+?>
+<!-- #flexbox -->
+<style>
+*{
+    box-sizing: border-box;
+}
+.contianer{
+    display:flex;
+    flex-wrap: wrap;
+    
+    margin-top: 1px;
+    margin-left: 1px;
+}
+.contianer>div{
+    background-color: white;
+    flex-basis: calc(100% / 7);
+    border: 1px solid black;
+    margin-top: -1px;
+    margin-left: -1px;
+    height:300px;
+}
+</style>
+<div class="contianer">
+    <?php 
+    for($i=0;$i<count($data);$i++){
+        echo "<div>";
+        echo $data[$i];
+        echo "</div>";
+    }
+   ?>
+    
+</div>
--- result.php
+++ result.php
@@ -1,24 +1,22 @@
 <?php
-$height=$_GET['height'];
-$weight=$_GET['weight'];
+$height = $_GET['height'];
+$weight = $_GET['weight'];
 
-echo "你輸入的身高為：".$height."公分<br>";
-echo "你輸入的體重為：".$weight."公斤<br>";
-$bmi=round($weight/(($height/100)*($height/100)));
-$level='';
-if($bmi>=27){
-    $level="肥胖";
-}else if($bmi>=24){
-    $lelve="體重過重";
-}else if($bmi>=18.5){
-    $level="建康體重";
-}else{
-    $level="體重過輕";
+echo "你輸入的身高為：" . $height . "公分<br>";
+echo "你輸入的體重為：" . $weight . "公斤<br>";
+$bmi = round($weight / (($height / 100) * ($height / 100)));
+$level = '';
+if ($bmi >= 27) {
+    $level = "肥胖";
+} else if ($bmi >= 24) {
+    $level = "體重過重";
+} else if ($bmi >= 18.5) {
+    $level = "建康體重";
+} else {
+    $level = "體重過輕";
 }
-echo "你的BMI為:".$bmi;
-echo"<br>";
-echo "你的體位判定為:".$level;
-echo"<br>";
+echo "你的BMI為:" . $bmi;
+echo "<br>";
+echo "你的體位判定為:" . $level;
+echo "<br>";
 echo "<a href='bmi_form.php'>回首頁</a>";
-
-
