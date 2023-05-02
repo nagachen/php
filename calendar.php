edb4392f70b4b4f395fd72b7b9f67853d41a72e5
nagachen <birdann99@gmail.com>
2023-05-02 16:33:35 +0800

    假日判斷做到一半

--- calendar.php
+++ calendar.php
@@ -16,14 +16,8 @@ $month_days = ($days + $first_week);      #當月總共要算幾天36
 $month_week = ceil($month_days/7);           #當月有幾週 6
 
 
-echo $first_day;
-echo"<br>";
-echo $days;
-    echo "<br>";
-echo $first_week;
-echo "<br>";
-echo $month .'月';
-echo "<br>";
+
+
 #建立月份陣列相關資料
 $data = [];
  for($i=0;$i<$month_week;$i++){
@@ -33,14 +27,12 @@ $data = [];
           if(($j<$first_week && $i == 0) ||($i==($month_week-1) && $final_week<$j)){
               $data[]="&nbsp;";
           }else{
-              $data[]=($i*7)+$j-$first_week+1;
+              $data[]=$year.'-'.$month.'-'.($i*7)+$j-$first_week+1;
     
           }
       }
  }
-// echo "<pre>";
-// print_r($data);
-// echo "</pre>";
+
 #判斷年月有沒有超過
 if($month==12){
     $nextmonth=1;
@@ -57,8 +49,12 @@ if($month==1){
     $prevyear=$year;
 }
 ?>
+<!--假日資料庫 -->
+<?php
+$holiday = ['5-1'=>'勞動節'];
+?>
 <!-- #flexbox -->
-
+<div class="years"><?=$year?>年</div>
 <!-- 上一月，這一月，下一月 -->
 <a href="calendar.php?year=<?=$prevyear;?>&month=<?=$prevmonth;?>"><?=$prevmonth;?>月</a>
 <a href="calendar.php?year=<?=$year;?>&month=<?=$month;?>"><?=$month;?>月</a>
@@ -74,7 +70,13 @@ if($month==1){
     <div class="tittle">星期六</div>
 
     <?php 
+
+    #使用新的data格式判斷日期
+    // 需要使用explode()取出$data[]日期和判斷空白
     for($i=0;$i<count($data);$i++){
+
+        $today=date('Y-n-j');
+        $d=($data[$i]=="&nbsp;")?'&nbsp;':explode($data[$i],'-')[2];  #取$i日期
         
         if($i%7==0 or $i%7==6){
             echo "<div class='holiday'>";
